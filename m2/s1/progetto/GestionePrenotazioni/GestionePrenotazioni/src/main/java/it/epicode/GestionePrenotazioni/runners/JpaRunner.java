6a8ee868149639b83fac95809e7fff05b08a494b
mirkoc94 <mirkocuciuffo@hotmail.it>
2024-05-18 11:56:37 +0200

    fine progetto gestione prenotazioni

--- m2/s1/progetto/GestionePrenotazioni/GestionePrenotazioni/src/main/java/it/epicode/GestionePrenotazioni/runners/JpaRunner.java
+++ m2/s1/progetto/GestionePrenotazioni/GestionePrenotazioni/src/main/java/it/epicode/GestionePrenotazioni/runners/JpaRunner.java
@@ -29,30 +29,47 @@ public class JpaRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         //creazione users
-        var user1 = users.save(User.builder().withUsername("mirko")
+        var u1 = users.save(User.builder().withUsername("mirko")
                 .withFirstName("mirko")
                 .withLastName("rossi")
                 .withEmail("mirkorossi@gmail.it")
                 .build());
-        var user2 = users.save(User.builder().withUsername("ale")
+        var u2 = users.save(User.builder().withUsername("ale")
                 .withFirstName("alex")
                 .withLastName("bianchi")
                 .withEmail("alebianchi@gmail.it")
                 .build());
 
         //creazione buildings
-        var build1 = buildings.save(Building.builder().withName("edificioA")
+        var b1 = buildings.save(Building.builder()
+                .withName("edificioA")
                 .withAddress("via Torino 1")
                 .withCity("ragusa")
                 .build());
-        var build2 = buildings.save(Building.builder().withName("edificioB")
+        var b2 = buildings.save(Building.builder()
+                .withName("edificioB")
                 .withAddress("viale Europa 2")
                 .withCity("modica")
                 .build());
 
         //creazione workstations
-        var workstation1 = workstations.save(Workstation.builder().withTypeWS(TypeWS.PRIVATE)
-                .withDescription("palazzo barocco, nei pressi della piazza centrale")
-                .withBuilding().build());
+        var ws1 = workstations.save(Workstation.builder()
+                .withTypeWS(TypeWS.PRIVATE)
+                .withDescription("auletta dotata di proiettore")
+                .withBuilding(b1)
+                .withMaxUsers(50)
+                .build());
+        var ws2 = workstations.save(Workstation.builder()
+                .withTypeWS(TypeWS.OPENSPACE)
+                .withDescription("annesso centro ristoro, situato al terzo piano dell'edificio")
+                .withBuilding(b2)
+                .withMaxUsers(80)
+                .build());
+        var ws3 = workstations.save(Workstation.builder()
+                .withTypeWS(TypeWS.MEETING_ROOM)
+                .withDescription("al piano terra dell'edificio, dotato di proiettori e microfoni")
+                .withBuilding(b2)
+                .withMaxUsers(120)
+                .build());
     }
 }
